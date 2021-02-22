9d469d96cce2da7a0473d8626159b1b10a37b4da
nakamurammm <mr.mar2.sor@gmail.com>
2021-02-22 02:27:04 +0000

    [4-10]add

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.page(params[:page]).reverse_order
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] ="Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
+end
